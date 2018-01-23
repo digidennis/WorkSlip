923c42571537b95e17265512bf3b6e2172e380ff
Dennis Jessen <digidennis3@gmail.com>
2018-01-23 14:01:19 +0100

    init

--- code/controllers/Adminhtml/WorkslipController.php
+++ code/controllers/Adminhtml/WorkslipController.php
@@ -5,7 +5,10 @@ class Digidennis_WorkSlip_Adminhtml_WorkslipController extends Mage_Adminhtml_Co
 
     public function indexAction()
     {
+        $this->_title($this->__('WorkSlip'))->_title($this->__('List'));
         $this->loadLayout();
+        $this->_setActiveMenu('digidennis/workslipgrid');
+        $this->_addContent($this->getLayout()->createBlock('digidennis_workslip/adminhtml_workslip'));
         $this->renderLayout();
     }
 }
