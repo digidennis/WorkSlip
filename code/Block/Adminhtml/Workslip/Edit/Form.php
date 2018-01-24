5907c595a9b5b099b1edba15fad4b14951fc0337
Dennis Jessen <digidennis3@gmail.com>
2018-01-24 20:57:56 +0100

    grid

--- code/Block/Adminhtml/Workslip/Edit/Form.php
+++ code/Block/Adminhtml/Workslip/Edit/Form.php
@@ -6,7 +6,7 @@ class Digidennis_WorkSlip_Block_Adminhtml_Workslip_Edit_Form extends Mage_Adminh
     {
         $form = new Varien_Data_Form(
             array(
-                'id' => 'workSlipEditForm',
+                'id' => 'workslipEditForm',
                 'action' => $this->getUrl('*/*/save', array('workslip_id' => $this->getRequest()->getParam('workslip_id'))
                 ),
                 'method' => 'post',
