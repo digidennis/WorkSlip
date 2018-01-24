17904b0c25206a06edc5ca8c1e1650a09e0891ad
Dennis Jessen <digidennis3@gmail.com>
2018-01-24 22:01:53 +0100

    grid

--- code/Block/Adminhtml/Workslip/Edit/Tab/Form.php
+++ code/Block/Adminhtml/Workslip/Edit/Tab/Form.php
@@ -7,13 +7,16 @@ class Digidennis_WorkSlip_Block_Adminhtml_Workslip_Edit_Tab_Form extends Mage_Ad
         $form = new Varien_Data_Form();
         $this->setForm($form);
         $customerfieldset = $form->addFieldset('customerFieldset', array('legend'=> $this->__('Customer')));
-        $customerfieldset->addField('registered', 'button', array(
-            'label' => Mage::helper('core')->__('Import from customer'),
-            'value' => Mage::helper('core')->__('Button Caption'),
-            'name'  => 'registered',
-            'class' => 'form-button',
-            'onclick' => "setLocation('{$this->getUrl('*/*/registeremail')}')",
-        ));
+
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+                'label'   => Mage::helper('core')->__('Import from customer'),
+                'onclick' =>'setLocation(\'{$this->getUrl(\'*/*/registeremail\')}\')',
+                'class'   =>'scalable add',
+            ));
+        $button->setName('import_customer');
+        $customerfieldset->setHeaderBar($button->toHtml());
+
         $customerfieldset->addField('firstname', 'text',
             array(
                 'label' => $this->__('Firstname'),
