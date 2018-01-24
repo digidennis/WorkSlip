4f0e8588b038f1b875b79655672c484f9a0c6e4e
Dennis Jessen <digidennis3@gmail.com>
2018-01-24 14:15:20 +0100

    grid

--- code/controllers/Adminhtml/WorkslipController.php
+++ code/controllers/Adminhtml/WorkslipController.php
@@ -38,7 +38,7 @@ class Digidennis_WorkSlip_Adminhtml_WorkslipController extends Mage_Adminhtml_Co
             $this->_addContent($this->getLayout()
                 ->createBlock('digidennis_workslip/adminhtml_workslip_edit'))
                 ->_addLeft($this->getLayout()
-                    ->createBlock('pfay_films/adminhtml_films_edit_tabs')
+                    ->createBlock('digidennis_workslip/adminhtml_workslip_edit_tabs')
                 );
             $this->renderLayout();
         }
