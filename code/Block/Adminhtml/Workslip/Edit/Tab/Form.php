61fe3c44bfd5445322150aca40ac74205ca3cb5b
Dennis Jessen <digidennis3@gmail.com>
2018-01-26 18:14:17 +0100

    grid

--- code/Block/Adminhtml/Workslip/Edit/Tab/Form.php
+++ code/Block/Adminhtml/Workslip/Edit/Tab/Form.php
@@ -72,7 +72,7 @@ class Digidennis_WorkSlip_Block_Adminhtml_Workslip_Edit_Tab_Form extends Mage_Ad
             array(
                 'class' => 'validate-date required-entry',
                 'align'=>"Bl",
-                'input_format' => Varien_Date::DATE_INTERNAL_FORMAT,
+                'input_format' => Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT),
                 'format' => Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT),
                 'label' => $this->__('Estimated Done Date'),
                 'required' => true,
