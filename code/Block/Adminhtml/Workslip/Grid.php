46f1efabf55f37b3d9a945ea98576a7ceaeb6808
Dennis Jessen <digidennis3@gmail.com>
2018-01-26 14:01:35 +0100

    grid

--- code/Block/Adminhtml/Workslip/Grid.php
+++ code/Block/Adminhtml/Workslip/Grid.php
@@ -13,10 +13,8 @@ class Digidennis_WorkSlip_Block_Adminhtml_Workslip_Grid extends Mage_Adminhtml_B
 
     protected function _prepareCollection()
     {
-        $collection = Mage::getModel('digidennis_workslip/workslip')
-            ->getCollection()
+        $collection = Mage::getResourceModel('digidennis_workslip/workslip_collection')
             ->addExpressionAttributeToSelect('name','CONCAT({{firstname}},"  ",{{lastname}}))', array('firstname','lastname'));
-
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
