4dd4b7e6bef337b03eaacb17be8fd1cec1a93ce0
Dennis Jessen <digidennis3@gmail.com>
2018-01-27 15:27:40 +0100

    materials grid

--- code/Block/Adminhtml/Workslip/Edit/Tabs.php
+++ code/Block/Adminhtml/Workslip/Edit/Tabs.php
@@ -19,13 +19,6 @@ class Digidennis_WorkSlip_Block_Adminhtml_Workslip_Edit_Tabs extends Mage_Adminh
                 ->createBlock('digidennis_workslip/adminhtml_workslip_edit_tab_form')
                 ->toHtml()
         ));
-        /*$this->addTab('materials', array(
-            'label' => $this->__('Materials'),
-            'title' => $this->__('Materials'),
-            'content' => $this->getLayout()
-                ->createBlock('digidennis_workslip/adminhtml_workslip_edit_tab_materials')
-                ->toHtml()
-        ));*/
 
         return parent::_beforeToHtml();
     }
