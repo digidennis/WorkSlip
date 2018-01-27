af7bf4cfb0ae9dcc2d909e394d374748dc777162
Dennis Jessen <digidennis3@gmail.com>
2018-01-27 14:36:56 +0100

    materials grid

---
+++ code/Model/Material.php
@@ -0,0 +1,9 @@
+<?php
+
+class Digidennis_WorkSlip_Model_Material extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('digidennis_workslip/material');
+    }
+}
