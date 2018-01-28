485abe2702b08fbfc98cf2d1e4f7159529010a8f
Dennis Jessen <digidennis3@gmail.com>
2018-01-28 16:07:20 +0100

    materials grid

--- code/Model/Workslip.php
+++ code/Model/Workslip.php
@@ -9,8 +9,8 @@ class Digidennis_WorkSlip_Model_Workslip extends Mage_Core_Model_Abstract
 
     protected function _afterLoad()
     {
-        if (is_null($this->getData('created_at'))) {
-            $this->setState(Mage::getSingleton('core/date')->gmtDate(now()));
+        if (is_null($this->getData('create_date'))) {
+            $this->setCreateDate(Mage::getSingleton('core/date')->gmtDate(now()));
         }
         return parent::_afterLoad();
     }
